0b60ab3511a3612e55eb89912a4a3276fc6ce589
thomaswang0822 <thomaswang0822@outlook.com>
2022-12-25 22:38:24 -0800

    fix no-detection bug

--- pages/WebAppModel.py
+++ pages/WebAppModel.py
@@ -71,7 +71,7 @@ if det_but and image is not None:
         yolo_model = Yolo_Predictor(MODEL_DIR, YOLO_DIR)
         img_input = yolo_model.load_img_from_stream(image)
         pred = yolo_model.one_prediction(img_input)
-        yolo_model.NMS_Draw(pred, img_input)
+        detected = yolo_model.NMS_Draw(pred, img_input)
         # yolo_model.display_img()
 
         # instead of display a pop-up window, we save the image. 
