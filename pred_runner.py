77be6705b6b16611ed547072f62b5ba325e91958
thomaswang0822 <thomaswang0822@outlook.com>
2022-12-28 11:48:43 -0800

    Add IMG_DIR choice

--- pred_runner.py
+++ pred_runner.py
@@ -3,11 +3,12 @@ from prediction import Yolo_Predictor
 if __name__ == "__main__":
     YOLO_DIR = "."
     MODEL_DIR = "./best.onnx"
-    fname = "background.jpg"
-    # fname = "000229.jpg"
+    IMG_DIR = "./test_data"
+    # fname = "background.jpg"
+    fname = "000229.jpg"
 
     yolo_model = Yolo_Predictor(MODEL_DIR, YOLO_DIR)
-    img_input = yolo_model.load_img(fname)
+    img_input = yolo_model.load_img(fname, img_dir=IMG_DIR)
     pred = yolo_model.one_prediction(img_input)
     yolo_model.NMS_Draw(pred, img_input)
     yolo_model.display_img()
