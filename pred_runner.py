12f043067981bc390a85b9a08032ce7325faa594
thomaswang0822 <thomaswang0822@outlook.com>
2022-12-29 23:41:49 -0800

    var rename

--- pred_runner.py
+++ pred_runner.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     prob_thold=0.5  # given this is an object of interest, what is the highest prob score of all classes
 
     yolo_model = Yolo_Predictor(MODEL_DIR, YOLO_DIR)
-    img_input = yolo_model.load_img(fname, img_dir=IMG_DIR)
+    img_input = yolo_model.load_data(fname, img_dir=IMG_DIR)
     pred = yolo_model.one_prediction(img_input)
     yolo_model.NMS_Draw(pred, img_input, conf_thold=conf_thold, prob_thold=prob_thold, YOLO_IMG_WH=YOLO_IMG_WH)
-    yolo_model.display_img()
+    yolo_model.display()
